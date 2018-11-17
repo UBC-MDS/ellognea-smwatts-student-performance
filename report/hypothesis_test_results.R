0ab12b1d811d1d98f0328cc2023c496228074ab9
Sarah Watts <smwatts@edu.uwaterloo.ca>
2018-11-17 14:19:37 -0800

    re-organized file struct

--- report/hypothesis_test_results.R
+++
@@ -1,8 +0,0 @@
-# hypothesis_test_results.R
-#
-# This will produce the results of the hypothesis test
-# This script takes no arguments.
-#
-# Usage: Rscript hypothesis_test_results.R
-
-print("Work in progress")
