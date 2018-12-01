c9099f4bb199dedffb7b1f407a3ed38243228c4f
ellognea <orpheliacarenne@gmail.com>
2018-12-01 12:15:46 -0800

    adding more meaningful comments

--- src/analysis_t-test_estimates.R
+++ src/analysis_t-test_estimates.R
@@ -2,10 +2,11 @@
 # Orphelia Ellogne, Oct 21 2018
 #
 # This script uses the clean data: data/clean-student-math-perf.csv 
-# It calculates the estimate and confidence intervals for each sample and to perform a t-test.
+# It calculates the estimate and confidence intervals for each sample and performs a t-test
 # The estimates and confidence intervals are provided by sample (male/female)
 # The t-test is performed on the null hypothesis:
 # mean final grade earned in math for male student = mean final grade earned in math for female students
+
 # This script takes in three arguments
 # 1. The argument for the path & filename to the clean dataset (i.e. "data/clean-student-math-perf.csv")
 # 2. The argument for the path & filename to the estimate and CI table (i.e. "results/estimate_table.csv")
@@ -25,9 +26,11 @@ results_table <- args[3]
 
 # define main function
 main <- function(){
+  
   # read in data
   student_math_perf <- read_csv(input_file)
-  # Estimate and CI table
+  
+  # calculate the estimate and confidence intervals for each sample and save output
   student_perf_est <- student_math_perf %>% 
     group_by(sex) %>% 
     summarize(
@@ -36,10 +39,11 @@ main <- function(){
   
   student_perf_est <- student_perf_est %>% mutate(ci_upper = mean_grade + 1.96*se,
                                                   ci_lower = mean_grade - 1.96*se)
-  # Perform two-tailed t-test
-  results <- broom::tidy(t.test(final_math_grade ~ sex, data=student_math_perf ,var.equal=FALSE))
-  #Save output
   write_csv(student_perf_est,estimate_table )
+  
+  # Perform two-tailed t-test and save output
+  results <- broom::tidy(t.test(final_math_grade ~ sex, data=student_math_perf ,var.equal=FALSE))
+  
   write_csv(results,results_table )
 }
 
